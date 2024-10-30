4ac302d1e481992379c28fa8eb4ef2f645560088
sworeman <sworeman@yandex.ru>
2024-10-30 08:16:32 +0300

    update

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -5,13 +5,26 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
 
     @GetMapping("/user")
     public String userPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        model.addAttribute("username", userDetails.getUsername());
+        String username = userDetails.getUsername();
+        User user = (User) userService.loadUserByUsername(username);
+        model.addAttribute("username", username);
+        model.addAttribute("roles", user.getRoles());
         return "user";
     }
 }
